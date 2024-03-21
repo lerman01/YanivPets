a1821ec8db19af14b062f5583fdab8f752a6d0d4
Yaniv <lerman01@gmail.com>
2024-03-21 22:10:54 +0200

    webrtc

--- js.js
+++ js.js
@@ -1,11 +1,16 @@
-addEventListener('DOMContentLoaded', ()=> {
+
+const test = () =>{
+    const peerConnection = connections.values().next().value
+    peerConnection.send('YANIV TEST')
+}
+addEventListener('DOMContentLoaded', () => {
     document.getElementById('startGame').onclick = () => {
-        ws.send('{"kind":"match-request","gameID":"https://rameshvarun.github.io/netplayjs/pong/","minPlayers":2,"maxPlayers":2}')
+        ws.send('{"kind":"match-request","gameID":"BLIBLA","minPlayers":2,"maxPlayers":2}')
     }
 })
 
 
-const connections = [];
+const connections = new Map();
 let iceServers = [];
 
 const signalingServerURL = 'wss://netplayjs.varunramesh.net/';
@@ -20,159 +25,18 @@ ws.onmessage = (message) => {
         // We've received a peer message. Check if we already have a
         // matching PeerConnection.
 
-        if (!connections.includes(msg.sourceID)) {
+        if (!connections.has(msg.sourceID)) {
             // Create the connection and emit it.
-            // const connection = new PeerConnection(this, msg.sourceID, false);
-            // this.connections.set(msg.sourceID, connection);
-            // this.onConnection.emit(connection);
-
-            const peerConnection = new RTCPeerConnection({
-                iceServers,
-            });
-
-            peerConnection.onicecandidate = (event) => {
-                console.log("AAAA")
-                if (event.candidate) {
-                    // this.client.send({
-                    //     kind: "send-message",
-                    //     type: "candidate",
-                    //     destinationID: peerID,
-                    //     payload: event.candidate,
-                    // });
-                }
-            };
-
-            peerConnection.onconnectionstatechange = (event) => {
-                console.log("BBBBB")
-                // log.debug(
-                //     `ConnectionStateChange: ${this.peerConnection.connectionState}`
-                // );
-                if (peerConnection.connectionState === "disconnected") {
-                    console.log("CCCCC")
-                    // this.close();
-                }
-            };
-
-            peerConnection.ondatachannel = (event) => {
-                console.log("FFFFFFFF")
-            };
-            connections.push(msg.sourceID)
+            const connection = new PeerConnection(ws, msg.sourceID, false);
+            connections.set(msg.sourceID, connection);
         }
-
         // Forward the signaling message to our peer.
-        //         this.connections
-        //             .get(msg.sourceID)!
-        //     .onSignalingMessage(msg.type, msg.payload);
+        connections.get(msg.sourceID).onSignalingMessage(msg.type, msg.payload);
     } else if (msg.kind === "host-match") {
-        //         // The server is telling us to host a match.
-        //         this.onHostMatch.emit({
-        //             clientIDs: msg.clientIDs,
-        //         });
 
-        // const peerConnection = new RTCPeerConnection({
-        //     iceServers,
-        // });
-        //
-        // peerConnection.onicecandidate = (event) => {
-        //         console.log("AAAA")
-        //     if (event.candidate) {
-        //         // this.client.send({
-        //         //     kind: "send-message",
-        //         //     type: "candidate",
-        //         //     destinationID: peerID,
-        //         //     payload: event.candidate,
-        //         // });
-        //     }
-        // };
-        //
-        // peerConnection.onconnectionstatechange = (event) => {
-        //     console.log("BBBBB")
-        //     // log.debug(
-        //     //     `ConnectionStateChange: ${this.peerConnection.connectionState}`
-        //     // );
-        //     if (peerConnection.connectionState === "disconnected") {
-        //         console.log("CCCCC")
-        //         // this.close();
-        //     }
-        // };
-        //
-        // peerConnection.ondatachannel = (event) => {
-        //     console.log("DDDDD")
-        // };
-        // peerConnection.onnegotiationneeded = async () => {
-        //     // Create an offer and send to our peer.
-        //     // const offer = await this.peerConnection.createOffer();
-        //     // this.client.send({
-        //     //     kind: "send-message",
-        //     //     type: "offer",
-        //     //     destinationID: peerID,
-        //     //     payload: offer,
-        //     // });
-        //     console.log('EEEEEEEEEEE')
-        // }
     } else if (msg.kind === "join-match") {
-        // The server is telling us to join a match.
-        const peerConnection = new RTCPeerConnection({
-            iceServers,
-        });
-
-        peerConnection.onicecandidate = (event) => {
-            console.log("AAAA")
-            // if (event.candidate) {
-            // this.client.send({
-            //     kind: "send-message",
-            //     type: "candidate",
-            //     destinationID: peerID,
-            //     payload: event.candidate,
-            // });
-            // }
-        };
-
-        peerConnection.onconnectionstatechange = (event) => {
-            console.log("BBBBB")
-            // log.debug(
-            //     `ConnectionStateChange: ${this.peerConnection.connectionState}`
-            // );
-            if (peerConnection.connectionState === "disconnected") {
-                console.log("CCCCC")
-                // this.close();
-            }
-        };
-
-        peerConnection.ondatachannel = (event) => {
-            console.log("DDDDD")
-        };
-
-        peerConnection.onnegotiationneeded = async () => {
-            // Create an offer and send to our peer.
-            console.log('Create offer for host')
-            const offer = await peerConnection.createOffer();
-            ws.send(JSON.stringify({
-                kind: "send-message",
-                type: "offer",
-                destinationID: msg.hostID,
-                payload: offer,
-            }));
-        }
-        const dataChannel = peerConnection.createDataChannel("data", {
-            ordered: true,
-        })
-
-        dataChannel.binaryType = "arraybuffer";
-        dataChannel.onopen = (e) => {
-            // this.emit("open");
-        };
-        dataChannel.onmessage = (e) => {
-            console.log("BABABABA")
-            // this.receiveStats.onMessage(e.data.byteLength);
-            // this.emit("data", msgpack.decode(new Uint8Array(e.data as ArrayBuffer)));
-        };
-
-        dataChannel.onclose = (e) => {
-            console.log("CDCDCDCD")
-            console.log("Data channel closed...");
-            // this.close();
-        };
+        const connection = new PeerConnection(ws, msg.hostID, true);
+        connections.set(msg.hostID, connection);
 
     }
 
---
+++ peerconnection.js
@@ -0,0 +1,129 @@
+
+/** A reliable data connection to a single peer. */
+class PeerConnection  {
+
+    constructor(client, peerID, initiator) {
+
+        this.client = client
+        this.peerID = peerID
+
+        // Create a RTCPeerConnection.
+        this.peerConnection = new RTCPeerConnection({
+            iceServers: client.iceServers
+        })
+
+        // Close the connection if the browser page is closed.
+        window.addEventListener("beforeunload", e => {
+            this.peerConnection.close()
+            this.dataChannel?.close()
+        })
+
+        // Send out candidate messages as we generate ICE candidates.
+        this.peerConnection.onicecandidate = event => {
+            if (event.candidate) {
+                this.client.send(JSON.stringify({
+                    kind: "send-message",
+                    type: "candidate",
+                    destinationID: peerID,
+                    payload: event.candidate
+                }))
+            }
+        }
+
+        this.peerConnection.onconnectionstatechange = event => {
+            console.log(`ConnectionStateChange: ${this.peerConnection.connectionState}`)
+            if (this.peerConnection.connectionState === "disconnected") {
+                this.close()
+            }
+        }
+
+        if (initiator) {
+            // Invoked when we are ready to negotiate.
+            this.peerConnection.onnegotiationneeded = async () => {
+                // Create an offer and send to our peer.
+                const offer = await this.peerConnection.createOffer()
+                this.client.send(JSON.stringify({
+                    kind: "send-message",
+                    type: "offer",
+                    destinationID: peerID,
+                    payload: offer
+                }))
+
+                // Install this offer locally.
+                await this.peerConnection.setLocalDescription(offer)
+            }
+
+            // Create a reliable data channel.
+            this.setDataChannel(
+                this.peerConnection.createDataChannel("data", {
+                    ordered: true
+                })
+            )
+        } else {
+            this.peerConnection.ondatachannel = event => {
+                this.setDataChannel(event.channel)
+            }
+        }
+    }
+
+    closed = false
+    close() {
+        if (!closed) {
+            this.closed = true
+            this.peerConnection.close()
+            this.dataChannel?.close()
+            this.onClose.emit()
+        }
+    }
+
+    setDataChannel(dataChannel) {
+        this.dataChannel = dataChannel
+        this.dataChannel.binaryType = "arraybuffer"
+        this.dataChannel.onopen = e => {
+            console.log("OPEN", e)
+            // this.emit("open")
+        }
+        this.dataChannel.onmessage = e => {
+            console.log("MESSAGE", e)
+            // this.receiveStats.onMessage(e.data.byteLength)
+            // this.emit("data", msgpack.decode(new Uint8Array(e.data)))
+        }
+        this.dataChannel.onclose = e => {
+            console.log("CLOSE", e)
+        }
+    }
+
+    async onSignalingMessage(type, payload) {
+        console.log(`onSignalingMessage: ${type}, ${JSON.stringify(payload)}`)
+        if (type === "offer") {
+            // Set the offer as our remote description.
+            await this.peerConnection.setRemoteDescription(payload)
+
+            // Generate an answer and set it as our local description.
+            console.log("Generating answer...")
+            const answer = await this.peerConnection.createAnswer()
+            await this.peerConnection.setLocalDescription(answer)
+
+            // Send the answer back to our peer.
+            this.client.send(JSON.stringify({
+                kind: "send-message",
+                type: "answer",
+                destinationID: this.peerID,
+                payload: answer
+            }))
+        } else if (type === "answer") {
+            // Set the answer as our remote description.
+            await this.peerConnection.setRemoteDescription(payload)
+        } else if (type === "candidate") {
+            // Add this ICE candidate.
+            await this.peerConnection.addIceCandidate(payload)
+        }
+    }
+
+    send(data) {
+        if (this.dataChannel?.readyState !== "open") return
+        // let encoded = msgpack.encode(data)
+        // this.sendStats.onMessage(encoded.byteLength)
+        this.dataChannel.send(data)
+    }
+}
