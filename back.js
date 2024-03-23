b763c00be80e866c7730ee11295ea0ab70c7a346
Yaniv <lerman01@gmail.com>
2024-03-23 18:07:10 +0200

    webrtc

--- back.js
+++ back.js
@@ -1,25 +1,37 @@
 addEventListener('DOMContentLoaded', () => {
-        const container = document.querySelector('#main-container')
-        const playersPlaces = [
-            document.querySelector('#right'),
-            document.querySelector('#top'),
-            document.querySelector('#left')
-        ]
-        const players = 13
-
-        const bottom = document.querySelector('#bottom');
-
-        const player = document.createElement('div')
-        player.textContent = `MY PLAYER`
-        player.classList.add('player')
-        bottom.appendChild(player)
-
-        for (let i = 0; i < players - 1; i++) {
-            const player = document.createElement('div')
-            player.textContent = `PLAYER ${i}`
-            player.classList.add('player')
-            playersPlaces[i % 3].appendChild(player)
-
-        }
+    createPlayers();
+    const input = document.querySelector('#player-counter')
+    input.onchange = () => {
+        createPlayers()
     }
-)
+})
+
+const createPlayers = (delta) => {
+    const input = document.querySelector('#player-counter')
+    input.value = Number(input.value) + delta
+    input.dispatchEvent(new Event('input', {bubbles: true}))
+
+    const players = input.value
+
+    const playersPlaces = [
+        document.querySelector('#top'),
+        document.querySelector('#right'),
+        document.querySelector('#left')
+    ]
+    const bottom = document.querySelector('#bottom');
+    bottom.innerHTML = ""
+    playersPlaces.forEach(place => place.innerHTML = "")
+
+    const player = document.createElement('div')
+    player.textContent = `MY PLAYER`
+    player.classList.add('player')
+    bottom.appendChild(player)
+
+    for (let i = 0; i < players - 1; i++) {
+        const player = document.createElement('div')
+        player.textContent = `PLAYER ${i}`
+        player.classList.add('player')
+        playersPlaces[i % 3].appendChild(player)
+
+    }
+}
